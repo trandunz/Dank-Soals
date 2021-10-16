814044c31cf0788401b3ffffab85b57a2c66c806
trandunz <54924605+trandunz@users.noreply.github.com>
2021-10-17 02:24:11 +1300

    + Basic Player Controller

---
+++ Dank-Soals/Assets/Scripts/Player/Script_Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Player : Script_CharacterMotor
+{
+    #region Singleton
+    private static Script_Player _instance;
+    public static Script_Player Instance;
+    #endregion
+
+    void Start()
+    {
+        InitCursorLockMode(CursorLockMode.Locked);
+        InitMotor();
+    }
+
+    void Update()
+    {
+        Motor();
+    }
+}
