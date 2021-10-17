fa14d7021d22792a49fa7ea37557c9781bcaf8ba
Trandunz <trandunz@gmail.com>
2021-10-17 22:01:56 +1300

    + Bullets And Particle Effects

--- Dank-Soals/Assets/Scripts/Script_AnimationEvents.cs
+++ Dank-Soals/Assets/Scripts/Script_AnimationEvents.cs
@@ -25,7 +25,7 @@ public class Script_AnimationEvents : MonoBehaviour
                     ScriptableObject_Weapon m_WeaponData = m_Player.m_WeaponWheel[1].GetComponent<Script_Weapon>().m_WeaponData;
                     m_Player.m_WeaponWheel[1].GetComponent<AudioSource>().PlayOneShot(m_WeaponData.UseSound);
 
-                    Rigidbody bullet = Instantiate(m_Player.m_WeaponWheel[0].GetComponent<Script_Weapon>().m_Bullet, m_Player.m_WeaponWheel[1].GetComponent<Script_Weapon>().m_Muzzle.transform.position, m_Player.m_WeaponWheel[1].GetComponent<Script_Weapon>().m_Muzzle.transform.rotation);
+                    Rigidbody bullet = Instantiate(m_Player.m_WeaponWheel[1].GetComponent<Script_Weapon>().m_Bullet, m_Player.m_WeaponWheel[1].GetComponent<Script_Weapon>().m_Muzzle.transform.position, m_Player.m_WeaponWheel[1].GetComponent<Script_Weapon>().m_Muzzle.transform.rotation);
                     bullet.velocity = m_Player.m_WeaponWheel[1].GetComponent<Script_Weapon>().m_Muzzle.transform.TransformDirection(new Vector3(0.0f, 0.0f, m_WeaponData.Bullet.GetComponent<Script_Ammunition>().m_TravelSpeed));
                     break;
                 }
--- Dank-Soals/Assets/Scripts/Weapons/Ammunition/Script_Ammunition.cs
+++ Dank-Soals/Assets/Scripts/Weapons/Ammunition/Script_Ammunition.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Script_Ammunition : MonoBehaviour
 {
     public float m_TravelSpeed = 1.0f;
-    [SerializeField] float m_Damage = 1.0f;
 
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] float m_Damage = 1.0f;
+    [SerializeField] AudioClip m_ImpactSound;
+
+    bool MARKASDESTROY = false;
+
+    void OnCollisionEnter(Collision collision)
     {
+        if (!MARKASDESTROY)
+        {
+            StartCoroutine(Impact());
+        }
+    }
+
+    IEnumerator Impact()
+    {
+        MARKASDESTROY = true;
+        GetComponent<MeshRenderer>().forceRenderingOff = true;
+        GetComponentInChildren<ParticleSystem>().Stop();
+        GetComponent<AudioSource>().PlayOneShot(m_ImpactSound);
+        yield return new WaitForSeconds(m_ImpactSound.length);
         Destroy(gameObject);
     }
 }
